16a1a0d014624f8bbda6baea475536dea1752d03
Maryana Antonyuk and Zema Kebede <zema@>
2019-04-10 16:05:26 -0700

    Add semi-functional logic. Program can display value of each roll but cannot add the values together

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
 
 //Back end ----
 //var roll = Math.floor( (Math.random() * 6)+1)
+
+var points = [];
+var sum = 0
 function roll() {
-  return Math.floor( (Math.random() * 6)+1)
+return Math.floor( (Math.random() * 6)+1)
 };
+
+function result(roll) {
+  if (roll == 1) {
+    console.log("turn over!");
+  }else {
+    points.push(roll);
+  }
+};
+
+function total() {
+  function getSum(total, num) {
+  return total + num;
+  console.log(points.reduce(getSum));
+}
+};
+
 $(document).ready(function() {
 
   $("button#roll").click(function() {
